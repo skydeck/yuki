decacbc42b0aba46b85ef79694703fdbd000da19
Matthew Tovbin <tovbinm@gmail.com>
2013-01-08 11:49:53 -0800

    Use the same connection in get and put in write_default call

--- src/client.ml
+++ src/client.ml
@@ -14,14 +14,15 @@ module Make(Conn:Make.Conn)(Elem:Make.Elem) = struct
   let keys = List.map (function { Rpb_link.key = Some key } -> key | _ -> raise Not_found)
   let links = List.map (fun key -> { Rpb_link.bucket = Some Elem.bucket; key = Some key; tag = None })
 
-  let get key = Conn.with_connection (fun conn ->
+  let get_conn key conn =
     match_lwt riak_get conn Elem.bucket key [] with
       | Some { obj_key = Some key; obj_value = Some value; obj_vclock = Some vclock; obj_links = links } ->
           return { key = key; value = Elem.of_string value; vclock = vclock; links = keys links }
       | _ -> raise Not_found
-  )
 
-  let put ?key ?v ?(ops=[Put_return_head true; Put_if_none_match true]) x ts = Conn.with_connection (fun conn ->
+  let get key = Conn.with_connection (get_conn key)
+
+  let put_conn ?key ?v ?(ops=[Put_return_head true; Put_if_none_match true]) x ts conn =
     match_lwt riak_put_raw conn Elem.bucket key ~links:(links ts) (Elem.to_string x) ops v with
       | Some { obj_key = Some key; obj_vclock = Some vclock } ->
           return { key = key; value = x; vclock = vclock; links = ts }
@@ -37,7 +38,9 @@ module Make(Conn:Make.Conn)(Elem:Make.Elem) = struct
             return { key = key; value = x; vclock = vclock; links = ts }
           | None -> raise Not_found)
       | _ -> raise Not_found
-  )
+
+  let put ?key ?v ?ops x ts =
+    Conn.with_connection (put_conn ?key ?v ?ops x ts)
 
   let read key fn =
     lwt { value = x } = get key in
@@ -57,13 +60,15 @@ module Make(Conn:Make.Conn)(Elem:Make.Elem) = struct
 
   let write_default key empty fn =
     try_lwt
-      lwt { value = x; vclock = v } = get key in
-      Printf.printf "[curr_head: %s]\n" (Elem.to_string x);
-      lwt x' = fn x in
-      Printf.printf "[new_head: %s]\n" (Elem.to_string x');
-      lwt { value = y; vclock = v' } = put ~key ~ops:[Put_return_body true] x' [] in
-      Printf.printf "write_default done. result: %s, old_vclock: %s, new_vclock: %s\n" (Elem.to_string y) v v';
-      return ()
+      Conn.with_connection (fun conn ->
+        lwt { value = x; vclock = v } = get_conn key conn in
+        Printf.printf "[curr_head: %s]\n" (Elem.to_string x);
+        lwt x' = fn x in
+        Printf.printf "[new_head: %s]\n" (Elem.to_string x');
+        lwt { value = y; vclock = v' } = put_conn ~key ~v ~ops:[Put_return_body true] x' [] conn in
+        Printf.printf "write_default done. result: %s, old_vclock: %s, new_vclock: %s\n" (Elem.to_string y) v v';
+        return ()
+      )
     with Not_found ->
       lwt x = fn empty in
       Printf.printf "[new_head: %s]\n" (Elem.to_string x);
